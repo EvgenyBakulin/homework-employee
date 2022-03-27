cee316b02bbcc27f4a07f808df3dc53a607cdcd0
Евгений <ugend84@yandex.ru>
2022-03-27 15:09:11 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homeworkemployee/HomeworkEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/homeworkemployee/HomeworkEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
